98c2af47cff0ca3e71231efc7d90f7c4d7aeb4a3
Akash Raj <akashsingh.77312@gmail.com>
2024-07-20 00:39:07 +0530

    Day 4

--- Day 4/add-two-numbers.java
+++ Day 4/add-two-numbers.java
@@ -3,3 +3,46 @@
 https://leetcode.com/problems/add-two-numbers/description/
 
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode head = ans;
+        int carry=0;
+        while(l1 != null && l2  != null){
+            int num = l1.val+l2.val+carry;
+            carry = num/10;
+            ans.next = new ListNode(num%10);
+            l1 = l1.next;
+            l2 = l2.next;
+            ans = ans.next;
+        }
+        while(l1 != null){
+            int num = l1.val+carry;
+            carry = num/10;
+            ans.next = new ListNode(num%10);
+            l1 = l1.next;
+            ans = ans.next;
+        }
+         while(l2 != null){
+            int num = l2.val+carry;
+            carry = num/10;
+            ans.next = new ListNode(num%10);
+            l2 = l2.next;
+            ans = ans.next;
+        }
+        if(carry>0){
+            ans.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+}
